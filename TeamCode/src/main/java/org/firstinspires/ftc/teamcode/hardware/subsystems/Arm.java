a2da8d54e7ab3171e8ae6b53e536600529ea7ea0
rishi763 <55810666+rishi763@users.noreply.github.com>
2022-12-16 17:04:33 -0500

    Add hardware code for Arm and Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Arm.java
@@ -1,10 +1,36 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystems;
 
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class Arm {
 
-    public Arm(HardwareMap hardwareMap){
+    private final double MIN_ANGLE=0;
+    private final double MAX_ANGLE=150;
 
+    private final ServoEx leftServo;
+    private final ServoEx rightServo;
+
+    public Arm(HardwareMap hardwareMap){
+        leftServo = new SimpleServo(hardwareMap, "leftServo", MIN_ANGLE, MAX_ANGLE, AngleUnit.DEGREES );
+        rightServo = new SimpleServo(hardwareMap, "rightServo", MIN_ANGLE, MAX_ANGLE, AngleUnit.DEGREES);
+
+        leftServo.setInverted(false);
+        rightServo.setInverted(false);
     }
+
+    public void rotateOpenPosition(){
+        leftServo.turnToAngle(MAX_ANGLE);
+        rightServo.turnToAngle(MAX_ANGLE);
+    }
+
+    public void rotateClosedPosition(){
+        leftServo.turnToAngle(MIN_ANGLE);
+        rightServo.turnToAngle(MIN_ANGLE);
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Claw.java
@@ -1,34 +1,47 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystems;
 
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImpl;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 
 public class Claw {
 
-    private final double OPEN_POSITION = 0.12;
-    private final double CLOSED_POSITION = 0.34;
+    private final double OPEN_GRAB_CLAW_ANGLE=0;
+    private final double CLOSE_GRAB_CLAW_ANGLE=60;
+    private final double OPEN_ROTATE_CLAW_ANGLE=135;
+    private final double CLOSE_ROTATE_CLAW_ANGLE=0;
 
-    private final Servo clawServo;
+
+    private final ServoEx rotateServo;
+    private final ServoEx grabServo;
 
     public Claw(HardwareMap hardwareMap){
-        clawServo = hardwareMap.get(Servo.class, "clawServo");
-        clawServo.setDirection(Servo.Direction.FORWARD);
-    }
-    public void openClaw() {
+        grabServo = new SimpleServo(hardwareMap, "grabServo",OPEN_GRAB_CLAW_ANGLE,CLOSE_GRAB_CLAW_ANGLE , AngleUnit.DEGREES);
+        grabServo.setInverted(false);
 
-        clawServo.setPosition(OPEN_POSITION);
-    }
-
-    public void closeClaw() {
-        clawServo.setPosition(CLOSED_POSITION);
+        rotateServo = new SimpleServo(hardwareMap, "rotateServo",OPEN_ROTATE_CLAW_ANGLE,CLOSE_ROTATE_CLAW_ANGLE , AngleUnit.DEGREES);
+        rotateServo.setInverted(false);
     }
 
-
-    public void periodic(){ //todo
-        return;
+    public void openGrabClaw(){
+        grabServo.turnToAngle(OPEN_GRAB_CLAW_ANGLE);
     }
+    public void closeGrabClaw(){
+        grabServo.turnToAngle(CLOSE_GRAB_CLAW_ANGLE);
+    }
+    public void openRotateClaw(){
+        rotateServo.turnToAngle(OPEN_ROTATE_CLAW_ANGLE);
+    }
+    public void closeRotateClaw(){
+        rotateServo.turnToAngle(CLOSE_ROTATE_CLAW_ANGLE);
+    }
+
+
 
 
 }
