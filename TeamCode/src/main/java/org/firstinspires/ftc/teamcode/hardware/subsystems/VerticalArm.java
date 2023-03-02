e1b96118954758cebb32d957afd20c2c6d96306b
Brozone0 <alexyou2006@outlook.com>
2023-03-02 14:07:53 -0500

    redo subsystem with more expandable and more readable code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/VerticalArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/VerticalArm.java
@@ -1,124 +1,112 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystems;
 
+import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-public class VerticalArm {
+public class VerticalArm extends SubsystemBase {
 
-    public enum ClawPosition {
-        OPEN,
-        CLOSED
+    //TODO: find init servo pos values
+    // make transfer mech actually work
+    public enum ClawPos {
+        INIT_POS(0.5),
+        OPEN_POS(0.75),
+        CLOSE_POS(0.5);
+
+        private double pos;
+        ClawPos(double pos) { this.pos = pos; }
+        public double getPos() { return pos; }
     }
 
-    public enum ArmPosition {
-        IN,
-        OUT
+    public enum ArmPos {
+        INIT_POS(0.0),
+        TRANSFER_POS(0.0),
+        OUTTAKE_POS(0.78);
+
+        private double pos;
+        ArmPos(double pos) { this.pos = pos; }
+        public double getPos() { return pos; }
     }
 
-    public enum RotateServoPosition {
-        UP,
-        DOWN
+    public enum HingePos {
+        INIT_POS(0.02),
+        OUTTAKE_POS(0.6),
+        TRANSFER_POS(0.02);
+
+        private double pos;
+        HingePos(double pos) { this.pos = pos; }
+        public double getPos() { return pos; }
     }
 
-    public enum PivotServoPosition {
-        PIVOTED,
-        UNPIVOTED
+    public enum PivotPos {
+        INIT_POS(0.67),
+        OUTTAKE_POS(0.0),
+        TRANSFER_POSS(0.67);
+
+        private double pos;
+        PivotPos(double pos) { this.pos = pos; }
+        public double getPos() { return pos; }
     }
-
-    //TODO: Tune claw values
-//0.05->0.25
-    private final double OPEN_CLAW_POSITION = 0.75;
-    private final double CLOSE_CLAW_POSITION = 0.5;
-
-    private final double ROTATE_CLAW_DOWN_POSITION = 0.02;
-    private final double ROTATE_CLAW_UP_POSITION = 0.6;
-
-    private final double ARM_OUT_POSITION = 0.78;
-    private final double ARM_IN_POSITION = 0.0;
-
-    private final double PIVOTED_CLAW_POSITION = 0.0;
-    private final double UNPIVOTED_CLAW_POSITION = 0.67;
 
     private final double CONE_INTERVAL = 0.04;
 
     private final Servo claw;
-    private final Servo rotateServo;
-    private final Servo armLeft;
-    private final Servo armRight;
-    private final Servo pivotServo;
+    private final Servo hinge;
+    private final Servo armL;
+    private final Servo armR;
+    private final Servo pivot;
 
-    public ArmPosition armPosition = ArmPosition.IN;
-    private ClawPosition clawPosition = ClawPosition.OPEN;
-    private RotateServoPosition rotateServoPosition = RotateServoPosition.UP;
-    private PivotServoPosition pivotServoPosition = PivotServoPosition.UNPIVOTED;
+    public ArmPos armPos = ArmPos.INIT_POS;
+    private ClawPos clawPos = ClawPos.INIT_POS;
+    private HingePos hingePos = HingePos.INIT_POS;
+    private PivotPos pivotPos = PivotPos.INIT_POS;
 
     public VerticalArm (HardwareMap hardwareMap) {
         claw = hardwareMap.get(Servo.class, "horizontalClaw");
-
-        // 3(Left), 4(Right)
-        armLeft = hardwareMap.get(Servo.class, "verticalArmLeft");
-        armRight = hardwareMap.get(Servo.class, "verticalArmRight");
-        rotateServo = hardwareMap.get(Servo.class, "verticalRotateServo");
-        pivotServo = hardwareMap.get(Servo.class, "verticalPivotServo");
-
-        armRight.setDirection(Servo.Direction.REVERSE);
+        armL = hardwareMap.get(Servo.class, "verticalArmLeft");
+        armR = hardwareMap.get(Servo.class, "verticalArmRight");
+        armR.setDirection(Servo.Direction.REVERSE);
+        hinge = hardwareMap.get(Servo.class, "verticalRotateServo");
+        pivot = hardwareMap.get(Servo.class, "verticalPivotServo");
     }
 
-    public void openClaw() {
-        claw.setPosition(OPEN_CLAW_POSITION);
-        clawPosition = ClawPosition.OPEN;
-    }
+    public void openClaw() { clawPos = ClawPos.OPEN_POS; }
 
-    public void closeClaw() {
-        claw.setPosition(CLOSE_CLAW_POSITION);
-        clawPosition = ClawPosition.CLOSED;
-    }
+    public void closeClaw() { clawPos = ClawPos.CLOSE_POS; }
 
-    public void extendArm() {
-        armLeft.setPosition(ARM_OUT_POSITION);
-        armRight.setPosition(1-ARM_OUT_POSITION);
-        armPosition = ArmPosition.OUT;
-    }
+    public void setArmOuttake() { armPos = ArmPos.OUTTAKE_POS; }
 
-    public void retractArm() {
-        armLeft.setPosition(ARM_IN_POSITION);
-        armRight.setPosition(1-ARM_IN_POSITION);
-        armPosition = ArmPosition.IN;
-    }
+    public void setArmTransfer() { armPos = ArmPos.TRANSFER_POS; }
 
-    public void uprightClaw() {
-        rotateServo.setPosition(ROTATE_CLAW_UP_POSITION);
-        rotateServoPosition = RotateServoPosition.UP;
-    }
+    public void setHingeOuttake() { hingePos = HingePos.OUTTAKE_POS; }
 
-    public void flipClaw() {
-        rotateServo.setPosition(ROTATE_CLAW_DOWN_POSITION);
-        rotateServoPosition = RotateServoPosition.DOWN;
-    }
+    public void setHingeTransfer() { hingePos = HingePos.TRANSFER_POS; }
 
-    public void pivotClaw() {
-        pivotServo.setPosition(PIVOTED_CLAW_POSITION);
-        pivotServoPosition = PivotServoPosition.PIVOTED;
-    }
+    public void setPivotOuttake() { pivotPos = PivotPos.OUTTAKE_POS; }
 
-    public void unpivotClaw() {
-        pivotServo.setPosition(UNPIVOTED_CLAW_POSITION);
-        pivotServoPosition = PivotServoPosition.UNPIVOTED;
-    }
+    public void setPivotTransfer() { pivotPos = PivotPos.TRANSFER_POSS; }
 
     public void clawToScore() {
         closeClaw();
-        pivotClaw();
-        uprightClaw();
-        extendArm();
+        setPivotOuttake();
+        setHingeOuttake();
+        setArmOuttake();
     }
     public void clawToTransfer() {
         closeClaw();
-        unpivotClaw();
-        flipClaw();
-        retractArm();
+        setPivotTransfer();
+        setHingeTransfer();
+        setArmTransfer();
+    }
+
+    public void setServoPoses() {
+        claw.setPosition(clawPos.getPos());
+        pivot.setPosition(pivotPos.getPos());
+        hinge.setPosition(hingePos.getPos());
+        armL.setPosition(armPos.getPos());
+        armR.setPosition(armPos.getPos());
     }
 
 }
